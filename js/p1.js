b18e466a242db7c7891e37c797f6bb4542ba687e
hector.rc@chihuahua2.tecnm.mx <cochechas@hotmail.com>
2024-11-13 16:05:53 -0600

    Practica1-consola

--- js/p1.js
+++ js/p1.js
@@ -1,4 +1,1 @@
-alert("En programación web, se quieren lucir,")
-alert("pero al ver el examen, se quieren morir.")
-alert("Entre tanto div, se van a marear,")
-alert("y si siguen así, los voy a reprobar.")
+console.log("Hola mundo")
---
+++ js/p2.js
@@ -0,0 +1,4 @@
+alert("En programación web, se quieren lucir,")
+alert("pero al ver el examen, se quieren morir.")
+alert("Entre tanto div, se van a marear,")
+alert("y si siguen así, los voy a reprobar.")
