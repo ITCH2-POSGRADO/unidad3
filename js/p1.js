f7402316e719475f99a9981594959c12d3a210d0
hector.rc@chihuahua2.tecnm.mx <cochechas@hotmail.com>
2024-11-13 15:28:19 -0600

    Practica1

--- js/p1.js
+++ js/p1.js
@@ -1,4 +1,4 @@
-alert("Hola mundo");
-alert("Hola profe como esta, jejeje");
-var producto = 'audifonos gamer';
-var auxiliar;
+alert("En programación web, se quieren lucir,")
+alert("pero al ver el examen, se quieren morir.")
+alert("Entre tanto div, se van a marear,")
+alert("y si siguen así, los voy a reprobar.")
